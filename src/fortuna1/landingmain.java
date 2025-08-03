8c6330242d2695a225039d871d262a57887d3be0
kianna365 <kiskisankian5@gmail.com>
2025-08-03 13:26:43 +0800

    added one

---
+++ src/fortuna1/landingmain.java
@@ -0,0 +1,54 @@
+package fortuna1;
+
+import java.util.Scanner;
+import bankingClass.bankingfortuna;
+
+public class landingmain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("HELLO WELCOME TO THE SYSTEM!");
+        System.out.println("What do you feel doing today? ");
+        System.out.println("1. Banking");
+        System.out.println("2. Doctors Appointment");
+        System.out.println("3. Shopping");
+        
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+        int attempts = 3;
+        
+        while(attempts > 0) {
+        switch(choice){
+            case 1:
+       
+                bankingfortuna bc = new bankingfortuna();
+                
+                System.out.println("Enter your Account No: ");
+                int accountNo = sc.nextInt();
+
+                System.out.println("Enter your Pin: ");
+                int pin = sc.nextInt();
+              
+                    if(bc.verifyAccount(accountNo, pin)){
+                        System.out.println("LOGIN SUCCESS");
+                    }else{
+                        attempts--;
+                        System.out.println("INVALID ACCOUNT!You have "+attempts+" left");
+                    }
+       
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            default:
+                System.out.println("Invalid Selection!");
+        
+        }
+            
+    }
+  }
+}
